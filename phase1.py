2e51cf244d0074d65bc7d071f45de98bbe9b6ef7
shivansh Gite <somnathgite123@gmail.com>
2025-04-18 17:57:13 +0530

    Update phase1.py

--- phase1.py
+++ phase1.py
@@ -1,10 +1,10 @@
-# Phase 1 libraries
+#  libraries
 import warnings
 import logging
 
 import streamlit as st
 
-# Phase 2 libraries
+#  libraries
 import os
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
